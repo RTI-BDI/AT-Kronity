d15e8f9a09e9b583b0e90ff1fce01e80a82ffff1
devis12 <43404864+devis12@users.noreply.github.com>
2022-01-26 10:53:57 +0100

    Delete Tesi/Assets/optic-master directory

--- Tesi/Assets/optic-master/optic/release/optic/CMakeFiles/doxygenoptic.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "doc/html"
-  "CMakeFiles/doxygenoptic"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/doxygenoptic.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
